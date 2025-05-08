81d50e060a6b58db6974fd1fc9d1b5de25d2b6a5
Muslim Hussaini <hussainimuslim2@gmail.com>
2025-05-08 13:47:37 -0400

    all starter code working

--- backend/database/utils/configDB.js
+++ backend/database/utils/configDB.js
@@ -4,9 +4,11 @@
 It declares and exports the variables for database name, username, and password.
 ==================================================*/
 // Declare the variables for database name, username, and password.
-const dbName = 'starter-server';
-const dbUser = 'postgres';
-const dbPwd = 'postgres';  // Note: If needed, change this password to match the password created for PostgreSQL database on the local machine.
+require('dotenv').config(); // Add this line at the top of your configDB.js
+
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPwd = process.env.DB_PASSWORD; // Note: If needed, change this password to match the password created for PostgreSQL database on the local machine.
 
 // Export the variables 
 module.exports = {
